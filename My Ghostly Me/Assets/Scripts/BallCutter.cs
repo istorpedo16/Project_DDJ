014d7018adbf4f09e69426d186e665575d34b893
DianaMSJesus <83040508+DianaMSJesus@users.noreply.github.com>
2022-01-28 23:48:00 +0000

    Jogo Quase Finalizado

--- My Ghostly Me/Assets/Scripts/BallCutter.cs
+++ My Ghostly Me/Assets/Scripts/BallCutter.cs
@@ -1,17 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BallCutter : MonoBehaviour
 {
-   public void OnTriggerEnter2D (Collider2D collider){
+    public bool isInRange;
+    void Update (){
        GameObject objectToDestroy = GameObject.FindGameObjectWithTag("LinkBall");
 
-        if(collider.GetComponent<CharacterController2D>() != null){
-            //if(Input.GetKeyDown(KeyCode.E)){
+        if(isInRange){
+            if(Input.GetKeyDown(KeyCode.C)){
                 Destroy (objectToDestroy);
                 
-            //}
+            }else if(Input.GetKeyDown(KeyCode.R)){
+                SceneManager.LoadScene("Game");
+            }
         }
    }
+
+   private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.gameObject.CompareTag("Toy")){
+            isInRange = true;
+            
+        }
+    }
+
+     private void OnTriggerExit2D(Collider2D collision) {
+        if(collision.gameObject.CompareTag("Toy")){
+            isInRange = false;
+            
+        }
+    }
 }
