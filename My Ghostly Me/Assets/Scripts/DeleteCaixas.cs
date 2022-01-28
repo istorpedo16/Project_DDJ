651d49003f184de50551e0a69a814a0eef204dad
DianaMSJesus <83040508+DianaMSJesus@users.noreply.github.com>
2022-01-28 00:57:58 +0000

    Mini Jogos
    Botão de abrir a porta feito assim como o que permite as caixas cairem

---
+++ My Ghostly Me/Assets/Scripts/DeleteCaixas.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteCaixas : MonoBehaviour
+{
+    public GameObject plantaforma;
+    public bool isInRange;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isInRange){
+
+            if(Input.GetKeyDown(KeyCode.E)){
+                
+                plantaforma.SetActive(false);
+            }
+            
+
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.gameObject.CompareTag("Toy")){
+            isInRange = true;
+            /*Debug.Log("Ghost in range");
+            textContent.SetText(interactKey.ToString(), true);
+            textContent.enabled = true;*/
+        }
+    }
+
+     private void OnTriggerExit2D(Collider2D collision) {
+        if(collision.gameObject.CompareTag("Toy")){
+            isInRange = false;
+            //Debug.Log("Ghost is NOT range");
+            //textContent.enabled = false;
+        }
+    }
+}
---
+++ My Ghostly Me/Assets/Scripts/PortaAbre.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortaAbre : MonoBehaviour
+{
+    public Collider2D pAzul;
+    public Collider2D pAmarelo;
+    public Collider2D pRosa;
+
+    public Collider2D boxAzul;
+    public Collider2D boxAmarela;
+    public Collider2D boxRosa;
+
+    public GameObject teste; //substituir pela porta
+
+    public bool isInRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pAzul.IsTouching(boxAzul) && pAmarelo.IsTouching(boxAmarela) && pRosa.IsTouching(boxRosa)){
+            if(Input.GetKeyDown(KeyCode.E)){
+                teste.SetActive(false);
+            }
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.gameObject.CompareTag("Toy")){
+            isInRange = true;
+            /*Debug.Log("Ghost in range");
+            textContent.SetText(interactKey.ToString(), true);
+            textContent.enabled = true;*/
+        }
+    }
+
+     private void OnTriggerExit2D(Collider2D collision) {
+        if(collision.gameObject.CompareTag("Toy")){
+            isInRange = false;
+            //Debug.Log("Ghost is NOT range");
+            //textContent.enabled = false;
+        }
+    }
+}
